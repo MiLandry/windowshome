d23e869a923dd8f79ba0115dfda4a7c1bb6e3868
Michael Landry <mlandryviolin@gmail.com>
2020-06-24 13:43:57 -0400

    scripts

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,20 @@
-echo "snthaoei"
+echo "Custom powershell scripts executing..."
+
+
 Set-Alias -Name gas -Value _gas
+
+Set-Alias -Name gdi -Value _gdi
+
+Set-Alias -Name com -Value _com
+
+Set-Alias -Name open-Value _open
+
+
+
 Function _gas {git status}
+
+Function _gdi {git diff}
+
+Function _com {git commit -m}
+
+Function _open {explorer .}
