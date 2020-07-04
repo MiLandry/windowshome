b05e6c48ceeb4402df0617e9c91d4cc6dd816b5d
Michael Landry <mlandryviolin@gmail.com>
2020-07-03 22:59:04 -0400

    add ise script

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,24 @@
+echo "Custom powershell scripts executing..."
+
+
+Set-Alias -Name gas -Value _gas
+
+Set-Alias -Name gdi -Value _gdi
+
+Set-Alias -Name com -Value _com
+
+Set-Alias -Name open-Value _open
+
+
+
+Function _gas {git status}
+
+Function _gdi {git diff}
+
+Function _com {
+    $Foo = $args[0]
+    "Foo: $Foo"
+     git commit -a -m "$Foo"
+}
+
+Function _open {explorer .}
